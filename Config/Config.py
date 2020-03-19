5cac6b7cd7a66e96b794444f4bbe116e80a7d861
liuchengxu <meiqianchifa@163.com>
2020-03-19 14:48:42 +0800

    2

--- Config/Config.py
+++ Config/Config.py
@@ -431,3 +431,17 @@ IM_json = [
 #         "鲶鱼pc发给钉钉 工作类型：工作通知 有接收人和部门id"),
 #
 # ]
+
+
+app_driver ={
+    'platformName':'Android',
+    'appPackage':'com.baidu.searchbox',
+    'appActivity':'com.baidu.searchbox.MainActivity',
+    "unicodeKeyboard":"True",# appium提供的一种输入法，可以传中文。测试时直接用这个输入法
+    "resetKeyboard":"True",# 程序结束时重置原来的输入法
+    "noReset":"True", # 不初始化手机app信息（类似不清楚缓存）
+    'automationName':'appium',
+    'deviceName':'c235f170',
+    'platformVersion':'9',
+    "autoGrantPermissions":"True" # 让appium自动授权app权限
+}
